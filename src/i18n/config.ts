bf48761b97fe9793731ff08dd0ab339a56ce2005
luandro <luandro@digital-democracy.org>
2025-06-24 15:44:50 -0300

    feat: add translations to Portuguese and Spanish defaulting to pt

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,17 +3,25 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslation from './locales/en.json';
+import ptTranslation from './locales/pt.json';
+import esTranslation from './locales/es.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'pt',
     debug: false,
     resources: {
+      pt: {
+        translation: ptTranslation
+      },
+      es: {
+        translation: esTranslation
+      },
       en: {
         translation: enTranslation
-      }
+      },
     },
     interpolation: {
       escapeValue: false
